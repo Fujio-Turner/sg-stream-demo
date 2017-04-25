d4af29ee61e8c4636954129ec0a98cad8bc74139
Fujio Turner <fujioturner@Fujios-Turner.local>
2017-04-25 13:36:49 -0500

    Saving App's Checkpoint is SG

--- python_sg_feed.py
+++ python_sg_feed.py
@@ -12,6 +12,21 @@ for line in r.iter_lines():
 		print line
 '''
 ### FILTER BY CHANNELS ###
-channels = 'bob,water'
+channels = 'bob,water,cake'
 url_param = url_param +'&filter=sync_gateway/bychannel&channels='+channels
+'''
+
+
+'''
+### Saving My App's Checkpoint inside Sync Gateway ###
+HTTP/1.1 PUT or GET
+Content-Type: application/json   
+http://{hostname}:4984/{DB}/_local/my_check_point_{ip address of app}
+{
+	"_id":"my_check_point_8.8.8.8",
+	"_rev":"1-0",
+	"datetime":"2016-12-04 17:34:57",
+	"seq":"5"	
+}
+Docs on local docs: https://developer.couchbase.com/documentation/mobile/1.4/references/sync-gateway/rest-api/index.html#/document
 '''
